05c97dedf95afe735dae489615e611e0261cfe7f
NicolasAndrey <110393195+NicolasAndrey@users.noreply.github.com>
2022-08-15 08:45:17 -0300

    Update cad_cliente.php

--- cad_cliente.php
+++ cad_cliente.php
@@ -1,25 +1,23 @@
 <html>
     <head>
-        <title>Cadastro do cliente</title>
-</head>
-<body bgcolor="geige">
-    <font color="green"><center>
-    <h1>CADASTRAR CLIENTE</h1>
-    <hr></font>
-    <form action="insere-cliente.php" method="post">
-        Nome : <input type="text" name="nome">
-        <br>
-        CPF : <input type="text" name="CPF">
-        <br>
-        Telefone : <input type="text" name="telefone">
-        <br>
-        Endereco : <input type="text" name="endereco">
-        <br>
-        <br><br>
-        <input type="submit" value="OK">
+        <title>Cadastro do Cliente</title>
+    </head>
+    <body bgcolor="geige">
+        <font color="green"><center>
+        <h1>CADASTRAR CLIENTE</h1>
+        <hr></font>
+        <form action="insere-cliente.php" method="post">
+            Nome : <input type="text" name="nome">
+            <br>
+            CPF : <input type="text" name="CPF">
+            <br>
+            Telefone : <input type="text" name="telefone">
+            <br>
+            Endereco : <input type="text" name="endereco">
+            <br>
+            <br><br>
+            <input type="submit" value="OK">
     </form>
     </center>
-</body>
-
-
-
+    </body>
+</html>
